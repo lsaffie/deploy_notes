55f65d9c4c182e6bdacccfc0c85d377198b9d721
Luis Saffie <luis@homestars.com>
2017-12-10 13:29:49 -0500

    views and rails engine

--- lib/deploy_notes.rb
+++ lib/deploy_notes.rb
@@ -3,18 +3,20 @@ require "deploy_notes/local"
 require "deploy_notes/cap"
 
 module DeployNotes
-  def self.get
-    deploy_note ||= deploy_type
-    deploy_note.get
-  end
+  class Engine < ::Rails::Engine
+    def self.get
+      deploy_note ||= deploy_type
+      deploy_note.get
+    end
 
-  private
+    private
 
-  def self.deploy_type
-    revision.blank? ? DeployNotes::Local.new : DeployNotes::Cap.new
-  end
+    def self.deploy_type
+      revision.blank? ? DeployNotes::Local.new : DeployNotes::Cap.new
+    end
 
-  def self.revision
-    `cat #{Rails.root.to_s}/REVISION | tr -d '\n'`
+    def self.revision
+      `cat #{Rails.root.to_s}/REVISION | tr -d '\n'`
+    end
   end
 end
